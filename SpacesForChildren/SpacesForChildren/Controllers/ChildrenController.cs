7e8289e6313d6a968b52acb25025e4608598b399
Rafa <henriques.rafa.filipe@gmail.com>
2018-01-06 02:28:54 +0000

    Update

--- SpacesForChildren/SpacesForChildren/Controllers/ChildrenController.cs
+++ SpacesForChildren/SpacesForChildren/Controllers/ChildrenController.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 using SpacesForChildren.Models;
 
 namespace SpacesForChildren.Controllers
 {
-
-    [Authorize(Roles = Profiles.Parent)]
     public class ChildrenController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -21,12 +17,7 @@ namespace SpacesForChildren.Controllers
         // GET: Children
         public ActionResult Index()
         {
-            var child = db.Child;
-            if (User.IsInRole("Pai"))
-            {
-                return View(db.Child.Include(c => c.Parent));
-
-            } 
+            var child = db.Child.Include(c => c.Parent);
             return View(child.ToList());
         }
 
@@ -42,6 +33,64 @@ namespace SpacesForChildren.Controllers
             {
                 return HttpNotFound();
             }
+            return View(child);
+        }
+
+        // GET: Children/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParentId = new SelectList(db.Users, "Id", "Name");
+            return View();
+        }
+
+        // POST: Children/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,DateOfBirth,ParentId")] Child child)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Child.Add(child);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentId = new SelectList(db.Users, "Id", "Name", child.ParentId);
+            return View(child);
+        }
+
+        // GET: Children/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Child child = db.Child.Find(id);
+            if (child == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParentId = new SelectList(db.Users, "Id", "Name", child.ParentId);
+            return View(child);
+        }
+
+        // POST: Children/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,DateOfBirth,ParentId")] Child child)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(child).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParentId = new SelectList(db.Users, "Id", "Name", child.ParentId);
             return View(child);
         }
 
